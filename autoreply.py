3babd086a0d1af57457418e690dfbe825edecba3
xnonhack <36075615+xnonhack@users.noreply.github.com>
2019-12-01 17:13:49 +0700

    Create autoreply.py

---
+++ autoreply.py
@@ -0,0 +1,30 @@
+from telethon import TelegramClient, events, sync
+
+from telethon.tl.functions.messages import GetHistoryRequest, GetBotCallbackAnswerRequest
+
+api_id = 949726
+api_hash = '3492d7632043d2e468baae99c5b283da'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+channel_uername = '@Fuja123'
+channel_entity = client.get_entity(channel_username)
+posts = client(GetHistoryRequest(
+    peer=channel_entity,
+    limit=1,
+    offset_date = None,
+    offset_id = 0,
+    max_id = 0,
+    min_id = 0,
+    add_offset = 0,
+    hash = 0
+    )
+)
+messageId = posts.messages[0].id
+client(GetBotCallbackAnswerRequest(
+    channel_username,
+    messageId,
+    data=posts.messages[0].reply_markup.rows[0].buttons[0].data
+    ))
+client.disconnect()
